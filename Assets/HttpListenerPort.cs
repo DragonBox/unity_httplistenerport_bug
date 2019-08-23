6c2b992769135dd3bf1f489d7cc9d0c4af62f823
Sebastien Cassard <s.cassard@wewanttoknow.com>
2019-08-23 15:22:53 +0200

    Now display pid of current program

--- Assets/HttpListenerPort.cs
+++ Assets/HttpListenerPort.cs
@@ -1,99 +1,115 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Net;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HttpListenerPort : MonoBehaviour {
+public class HttpListenerPort : MonoBehaviour
+{
 
-	HttpListener listener;
+    HttpListener listener;
 
-	[SerializeField]
-	Button StartButton;
-	[SerializeField]
-	Button StopButton;
-	[SerializeField]
-	Text URI;
-	[SerializeField]
-	 Text Status;
-	
-	void Start()
-	{
-		URI.text = "http://127.0.0.1:12345/";
-	}
+    [SerializeField]
+    Button StartButton;
+    [SerializeField]
+    Button StopButton;
+    [SerializeField]
+    Text URI;
+    [SerializeField]
+    Text Status;
+    [SerializeField]
+    Text Pid;
 
-	public void DoStart () {
-		listener = new HttpListener();
-		string uriprefix = URI.text;
-		/* if (uriprefix.Length == 0)
+
+
+    void Start()
+    {
+        URI.text = "http://127.0.0.1:12345/";
+
+        Pid.text = "Pid : " + Process.GetCurrentProcess().Id.ToString();
+        UnityEngine.Debug.Log(Pid.text);
+    }
+
+    public void DoStart()
+    {
+        listener = new HttpListener();
+        string uriprefix = URI.text;
+        /* if (uriprefix.Length == 0)
 			uriprefix =  ;*/
-		Debug.Log("Starting with prefix: " + uriprefix);
-		listener.Prefixes.Add(uriprefix);
-		listener.Start();
-		Debug.Log("IsListening: " + listener.IsListening);
-		
-		IPEndPoint endpoint = CreateListenerRequest(listener, uriprefix);
-		Debug.Log("Using port : " + endpoint.Port);
-	}
-	public IPEndPoint CreateListenerRequest (HttpListener listener, string uri)
-		{
-			IPEndPoint ipEndPoint = null;
-			var mre = new System.Threading.ManualResetEvent (false);
-			listener.BeginGetContext (result => {
-				ipEndPoint = ListenerCallback (result);
-				mre.Set ();
-			}, listener);
+        UnityEngine.Debug.Log("Starting with prefix: " + uriprefix);
+        listener.Prefixes.Add(uriprefix);
+        listener.Start();
+        UnityEngine.Debug.Log("IsListening: " + listener.IsListening);
 
-			var request = (HttpWebRequest) WebRequest.Create (uri);
-			request.Method = "POST";
+        IPEndPoint endpoint = CreateListenerRequest(listener, uriprefix);
+        UnityEngine.Debug.Log("Using port : " + endpoint.Port);
+    }
+    public IPEndPoint CreateListenerRequest(HttpListener listener, string uri)
+    {
+        IPEndPoint ipEndPoint = null;
+        var mre = new System.Threading.ManualResetEvent(false);
+        listener.BeginGetContext(result =>
+        {
+            ipEndPoint = ListenerCallback(result);
+            mre.Set();
+        }, listener);
 
-			// We need to write something
-			request.GetRequestStream ().Write (new byte [] {(byte)'a'}, 0, 1);
-			request.GetRequestStream ().Dispose ();
+        var request = (HttpWebRequest)WebRequest.Create(uri);
+        request.Method = "POST";
 
-			// Send request, socket is created or reused.
-			var response = request.GetResponse ();
+        // We need to write something
+        request.GetRequestStream().Write(new byte[] { (byte)'a' }, 0, 1);
+        request.GetRequestStream().Dispose();
 
-			Debug.Log("HI: " + response.ResponseUri);
+        // Send request, socket is created or reused.
+        var response = request.GetResponse();
 
-			// Close response so socket can be reused.
-			response.Close ();
+        UnityEngine.Debug.Log("HI: " + response.ResponseUri);
 
-			mre.WaitOne();
+        // Close response so socket can be reused.
+        response.Close();
 
-			return ipEndPoint;
-		}
+        mre.WaitOne();
 
-		public static IPEndPoint ListenerCallback (IAsyncResult result)
-		{
-			var listener = (HttpListener) result.AsyncState;
-			var context = listener.EndGetContext (result);
-			var clientEndPoint = context.Request.RemoteEndPoint;
+        return ipEndPoint;
+    }
 
-			// Disposing InputStream should not avoid socket reuse
-			context.Request.InputStream.Dispose ();
+    public static IPEndPoint ListenerCallback(IAsyncResult result)
+    {
+        var listener = (HttpListener)result.AsyncState;
+        var context = listener.EndGetContext(result);
+        var clientEndPoint = context.Request.RemoteEndPoint;
 
-			// Close OutputStream to send response
-			context.Response.OutputStream.Close ();
+        // Disposing InputStream should not avoid socket reuse
+        context.Request.InputStream.Dispose();
 
-			return clientEndPoint;
-		}
+        // Close OutputStream to send response
+        context.Response.OutputStream.Close();
 
-	void Update() {
-		if (listener != null && listener.IsListening) {
-			Status.text = "Started";
-		}
-		else {
-			Status.text = "Stopped";
-		}
-		StartButton.gameObject.SetActive(listener == null || !listener.IsListening);
-		StopButton.gameObject.SetActive(listener != null && listener.IsListening);
-	}
-	
-	public void DoStop() {
-		if (listener != null) {
-			Debug.Log("Stopping listener");
-			listener.Stop();
-		}
-		listener = null;
-	}
+        return clientEndPoint;
+    }
+
+    void Update()
+    {
+        if (listener != null && listener.IsListening)
+        {
+            Status.text = "Started";
+        }
+        else
+        {
+            Status.text = "Stopped";
+        }
+        StartButton.gameObject.SetActive(listener == null || !listener.IsListening);
+        StopButton.gameObject.SetActive(listener != null && listener.IsListening);
+    }
+
+    public void DoStop()
+    {
+        if (listener != null)
+        {
+            UnityEngine.Debug.Log("Stopping listener");
+            listener.Stop();
+        }
+        listener = null;
+    }
 }
