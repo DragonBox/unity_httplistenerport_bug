e6ab73895d250a3980fdc5d335a19e7e266aeb3b
Sebastien Cassard <s.cassard@wewanttoknow.com>
2019-08-23 17:26:37 +0200

    Opened port by current process are now displayed

--- Assets/HttpListenerPort.cs
+++ Assets/HttpListenerPort.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,6 +23,8 @@ public class HttpListenerPort : MonoBehaviour
     [SerializeField]
     Text Pid;
 
+    [SerializeField]
+    Text OpenedPort;
 
 
     void Start()
@@ -111,5 +116,108 @@ public class HttpListenerPort : MonoBehaviour
             listener.Stop();
         }
         listener = null;
+    }
+
+    public void DoGetPorts()
+    {
+        OpenedPort.text = "";
+        List<Port> ports = GetNetStatPorts();
+        foreach (Port port in ports)
+        {
+            if (port.pid == Process.GetCurrentProcess().Id)
+            {
+                OpenedPort.text = OpenedPort.text + port.port_number + " ";
+            }
+            //UnityEngine.Debug.Log("Ports : " + port.pid.ToString());
+        }
+    }
+
+    // ===============================================
+    // The Method That Parses The NetStat Output
+    // And Returns A List Of Port Objects
+    // ===============================================
+    public static List<Port> GetNetStatPorts()
+    {
+        var Ports = new List<Port>();
+        try
+        {
+            using (Process p = new Process())
+            {
+
+                ProcessStartInfo ps = new ProcessStartInfo();
+                ps.Arguments = "-a -n -o";
+                ps.FileName = "netstat.exe";
+                ps.UseShellExecute = false;
+                ps.WindowStyle = ProcessWindowStyle.Hidden;
+                ps.RedirectStandardInput = true;
+                ps.RedirectStandardOutput = true;
+                ps.RedirectStandardError = true;
+
+                p.StartInfo = ps;
+                p.Start();
+
+                StreamReader stdOutput = p.StandardOutput;
+                StreamReader stdError = p.StandardError;
+
+                string content = stdOutput.ReadToEnd() + stdError.ReadToEnd();
+                string exitStatus = p.ExitCode.ToString();
+
+                if (exitStatus != "0")
+                {
+                    // Command Errored. Handle Here If Need Be
+                }
+
+                //Get The Rows
+                string[] rows = Regex.Split(content, "\r\n");
+                foreach (string row in rows)
+                {
+                    //Split it baby
+                    string[] tokens = Regex.Split(row, "\\s+");
+                    if (tokens.Length > 4 && (tokens[1].Equals("UDP") || tokens[1].Equals("TCP")))
+                    {
+                        string localAddress = Regex.Replace(tokens[2], @"\[(.*?)\]", "1.1.1.1");
+                        Ports.Add(new Port
+                        {
+                            protocol = localAddress.Contains("1.1.1.1") ? String.Format("{0}v6", tokens[1]) : String.Format("{0}v4", tokens[1]),
+                            port_number = localAddress.Split(':')[1],
+                            process_name = tokens[1] == "UDP" ? LookupProcess(Convert.ToInt16(tokens[4])) : LookupProcess(Convert.ToInt16(tokens[5])),
+                            pid = Convert.ToInt16(tokens[5])
+                        });
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return Ports;
+    }
+
+    public static string LookupProcess(int pid)
+    {
+        string procName;
+        try { procName = Process.GetProcessById(pid).ProcessName; }
+        catch (Exception) { procName = "-"; }
+        return procName;
+    }
+
+    // ===============================================
+    // The Port Class We're Going To Create A List Of
+    // ===============================================
+    public class Port
+    {
+        public string name
+        {
+            get
+            {
+                return string.Format("{0} ({1} port {2})", this.process_name, this.protocol, this.port_number);
+            }
+            set { }
+        }
+        public string port_number { get; set; }
+        public string process_name { get; set; }
+        public string protocol { get; set; }
+        public int pid { get; set; }
     }
 }
